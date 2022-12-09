d4970b243df211a004bf9b5a6d4c95e006a791bd
Zac <zacheryngky@gmail.com>
2022-12-09 16:31:21 +0800

    Toggles active tab when button is clicked

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import './styles.css';
 import { createMenuPage } from './menu-page/menu';
 
 const body = document.body;
+const tabContainer = document.getElementById("tab-container");
+const tabs = tabContainer.querySelectorAll("button");
+
+tabContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tab") && !e.target.classList.contains("active")) {
+        for (let tab of tabs) {
+            if (tab !== e.target) {
+            tab.classList.remove("active")
+            } else {
+                tab.classList.add("active");
+            }
+        }
+    }
+});
+
 body.appendChild(createMenuPage());
