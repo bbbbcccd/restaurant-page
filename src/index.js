fa6cdd75ee14e80e34f1544c21295098981c84ba
Zac <zacheryngky@gmail.com>
2022-12-09 17:04:24 +0800

    Load the correct page when tab switches

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 import './styles.css';
 import { createMenuPage } from './menu-page/menu';
 
-const body = document.body;
+const body = document.getElementsByTagName("body")[0];
 const tabContainer = document.getElementById("tab-container");
 const tabs = tabContainer.querySelectorAll("button");
 
 tabContainer.addEventListener("click", (e) => {
-    if (e.target.classList.contains("tab") && !e.target.classList.contains("active")) {
+    const target = e.target;
+    if (target.classList.contains("tab") && !target.classList.contains("active")) {
         for (let tab of tabs) {
-            if (tab !== e.target) {
+            if (tab !== target) {
             tab.classList.remove("active")
             } else {
                 tab.classList.add("active");
             }
         }
+        removeContent();
+        addContent(e.target.id);   
     }
 });
+
+function removeContent() {
+    const content = document.getElementById("content");
+    if (content !== null) {
+        body.removeChild(content);
+    }
+}
+
+function addContent(activeTab) {
+    if (activeTab === "menu") {
+        body.appendChild(createMenuPage());
+    }     
+}
 
 body.appendChild(createMenuPage());
