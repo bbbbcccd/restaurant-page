a1a1a3b01f495e38435bc9688a64c316b4b499f0
Zac <zacheryngky@gmail.com>
2022-12-13 17:19:01 +0800

    Update createReviewPage to display form
    Add setMultipleAttributes function to set multiple attributes to an
    element at once.
    
    Add createForm function
    
    [Ticket: X]

--- src/review-page/review.js
+++ src/review-page/review.js
@@ -1,7 +1,45 @@
 import './review.css';
 
+function setMultipleAttributes(el, attributes) {
+    Object.keys(attributes).forEach(attr => {
+        el.setAttribute(attr, attributes[attr]);
+    });
+}
+
 function createReviewPage() {
-    console.log("Hello review page");
+    const content = document.createElement("div");
+    content.setAttribute('id', 'content');
+
+    content.appendChild(createForm());
+    return content;
+}
+
+function createForm() {
+    const form = document.createElement("form");
+    const formAttributes = {
+        'action': '?',
+        'method': 'post', 
+    };
+    setMultipleAttributes(form, formAttributes);
+    
+    const input = document.createElement("input");
+    const inputAttributes = {
+        'type': 'text',
+        'id': 'add-review',
+        'name': 'add-review',
+        'maxlength': '150',
+        'autocomplete': 'off',
+        'placeholder': 'Add a review',
+    }
+    setMultipleAttributes(input, inputAttributes);
+    form.appendChild(input);
+
+    const submitBtn = document.createElement("button");
+    submitBtn.setAttribute("type", "submit");
+    submitBtn.textContent = "POST";
+    form.appendChild(submitBtn);
+
+    return form;
 }
 
 export { createReviewPage };
